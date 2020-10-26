6b222948a2680c19d7a3dab53f0b3d76c5c3d364
RSTEAM Solutions <info@rsteam.si>
2020-10-26 08:00:59 +0100

    Update to 1.3.2

--- src/xcode/ENA/ENA/Source/Scenes/ENSetting/ENStateHandler.swift
+++ src/xcode/ENA/ENA/Source/Scenes/ENSetting/ENStateHandler.swift
@@ -34,45 +34,17 @@ final class ENStateHandler {
 	var state: ENStateHandler.State {
 		currentState
 	}
-	
-	private let reachabilityService: ReachabilityService
+
 	private weak var delegate: ENStateHandlerUpdating?
-	private var internetOff = false
 	private var latestExposureManagerState: ExposureManagerState
 	
 	init(
 		initialExposureManagerState: ExposureManagerState,
-		reachabilityService: ReachabilityService,
 		delegate: ENStateHandlerUpdating
 	) {
-		self.reachabilityService = reachabilityService
 		self.delegate = delegate
 		self.latestExposureManagerState = initialExposureManagerState
 		self.currentState = determineCurrentState(from: latestExposureManagerState)
-		self.reachabilityService.observe(on: self) { [weak self] reachabilityState in
-			self?.internet(reachabilityState == .connected)
-		}
-	}
-
-	private func internet(_ isReachable: Bool) {
-		if !isReachable {
-			internetOff = true
-		} else {
-			internetOff = false
-		}
-
-		switch currentState {
-		case .disabled, .bluetoothOff, .restricted, .notAuthorized, .unknown:
-			return
-		case .enabled:
-			if !isReachable {
-				currentState = .internetOff
-			}
-		case .internetOff:
-			currentState = determineCurrentState(from: latestExposureManagerState)
-		case .none:
-			fatalError("Unexpected state found in ENState Handler")
-		}
 	}
 
 	private func stateDidChange() {
@@ -86,9 +58,6 @@ final class ENStateHandler {
 
 		switch enManagerState.status {
 		case .active:
-			guard !internetOff else {
-				return .internetOff
-			}
 			return .enabled
 		case .bluetoothOff:
 			guard !enManagerState.enabled == false else {
@@ -101,8 +70,11 @@ final class ENStateHandler {
 			return differentiateRestrictedCase()
 		case .unknown:
 			return .disabled
+		case .paused:
+			return .disabled
 		@unknown default:
-			fatalError("New state was added that is not being covered by ENStateHandler")
+			logError(message: "New state was added that is not being covered by ENStateHandler")
+			return .unknown
 		}
 	}
 
@@ -117,7 +89,8 @@ final class ENStateHandler {
 		case .authorized:
 			return .disabled
 		@unknown default:
-			fatalError("New state was added that is not being covered by ENStateHandler")
+			logError(message: "New state was added that is not being covered by ENStateHandler")
+			return .unknown
 		}
 	}
 }
